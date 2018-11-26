cc1d9e4c7ac0638b5122a166ad42fd287b496572
Elena Ponomarenko <elen.jellyfish@gmail.com>
2018-11-26 17:23:56 +0300

    init

---
+++ src/ua/hillel/Ponomarenko/Lesson3/task3/Util.java
@@ -0,0 +1,21 @@
+package ua.hillel.Ponomarenko.Lesson3.task3;
+
+public class Util {
+    public static void checkPalindrome(String str) {
+        String strWithoutSpaces = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                strWithoutSpaces += str.charAt(i);
+            }
+        }
+        String reversedString = new StringBuffer(strWithoutSpaces).reverse().toString();
+        String sub1 = strWithoutSpaces.substring(0, strWithoutSpaces.length() / 2);
+        String sub2 = reversedString.substring(0, reversedString.length() / 2);
+        if (sub1.equalsIgnoreCase(sub2)) {
+            System.out.println("This string is palindrome");
+        } else {
+            System.out.println("This string is not palindrome");
+        }
+
+    }
+}
