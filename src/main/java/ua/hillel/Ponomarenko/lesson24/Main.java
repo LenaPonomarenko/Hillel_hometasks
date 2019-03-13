b1602b606c13d692100351a4735886767a036033
Elena Ponomarenko <elen.jellyfish@gmail.com>
2019-03-13 15:38:25 +0300

    lesson 24

--- src/main/java/ua/hillel/Ponomarenko/lesson24/Main.java
+++ src/main/java/ua/hillel/Ponomarenko/lesson24/Main.java
@@ -2,17 +2,26 @@ package ua.hillel.Ponomarenko.lesson24;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ua.hillel.Ponomarenko.lesson23.Animal;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws JsonProcessingException {
-        Map map = new HashMap();
-        map.put("Vanya", "sadasd");
-        ObjectMapper mapper = new ObjectMapper();
-        String str = mapper.writeValueAsString(map);
-        System.out.println(str);
+    public static void main(String[] args) throws IOException {
+        Animal bear = new Animal(1,"Misha");
+        ObjectMapper mapper1 = new ObjectMapper();
+        String string = mapper1.writeValueAsString(bear);
+        FileWriter file = new FileWriter("D:/hillel/tasks/src/main/resources/JSONobject.json");
+        try {
+            file.write(string);
+            System.out.println("JSON object: "+string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            file.flush();
+            file.close();
+        }
     }
 }
